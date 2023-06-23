e7009ae39771756546c8e9a5021c2d91708f6f65
Willian <willian.amorim1@outlook.com>
2023-06-23 17:30:05 -0300

    concluido requisito 01 do projeto

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,18 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    
+    quantidade = 0
+    
+    for estudante in permanence_period:
+        estudante_min, estudante_max = estudante
+
+        if type(estudante_min) != int:
+            return None
+        
+        if type(estudante_max) != int:
+            return None
+            
+        if estudante_min <= target_time <= estudante_max:
+            quantidade += 1
+    return quantidade
