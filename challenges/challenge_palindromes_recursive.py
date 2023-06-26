dcac5b422ddb54deb4c39f79b827298961ce19a1
Willian <willian.amorim1@outlook.com>
2023-06-26 17:42:05 -0300

    resolvido dois problemas de lint

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -4,6 +4,5 @@ def is_palindrome_recursive(word, low_index, high_index):
     if low_index >= high_index:
         return True
     if word[low_index] == word[high_index]:
-        return is_palindrome_recursive(word, low_index + 1, high_index -1)
-    
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     return False
