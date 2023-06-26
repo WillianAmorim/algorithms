50c60652e349f03c7b56c352c82aba52a37972a6
Willian <willian.amorim1@outlook.com>
2023-06-26 17:10:15 -0300

    feito requisito 2 do projeto

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("string", "number")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):    
+        encrypt_message(1, 1)
+
+    result = encrypt_message('Tryber', 3)
+    assert result == "yrT_reb"
+
+    result = encrypt_message('Tryber', 4)
+    assert result == "re_byrT"
+
+    result = encrypt_message('Tryber', 10)
+    assert result == "rebyrT"
