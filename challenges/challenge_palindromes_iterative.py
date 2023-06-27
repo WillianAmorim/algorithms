fc224be40a2d0d3e0503db9532526e2ea93d7622
Willian <willian.amorim1@outlook.com>
2023-06-27 20:17:28 -0300

    resolvido desafio 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,7 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == '':
+        return False
+    for i in range(len(word)):
+        if word[i] != word[len(word) - i - 1]:
+            return False
+    return True
