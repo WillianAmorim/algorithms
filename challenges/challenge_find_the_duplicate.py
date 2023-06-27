eb709e6235898a841505d860d9d088702fbcb6bf
Willian <willian.amorim1@outlook.com>
2023-06-27 19:48:57 -0300

    resolvido desafio 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or len(nums) <= 1:
+        return False
+    nums.sort()
+
+    for i, num in enumerate(nums):
+        if isinstance(num, str) or num < 0:
+            return False
+        if i > 0 and num == nums[i - 1]:
+            return num
+    return False
